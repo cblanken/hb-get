4fde0a969f22867bf37cc8f87f61c3676e7351c3
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-04-14 16:50:11 -0400

    Add argparsing

--- hb-get.py
+++ hb-get.py
@@ -1,32 +1,43 @@
 #!/bin/python
+import argparse
+import os
+import pathlib
 import sys
-from os import path, makedirs
-from requests import get
+import requests
 from bs4 import BeautifulSoup
 
+parser = argparse.ArgumentParser(
+        prog="hb-get",
+        description="Humble Book Bundle download script",
+        epilog="")
+
+parser.add_argument("-o", "--output_dir", nargs="?", default=pathlib.Path("./output/"), help="Output directory")
+parser.add_argument("-i", "--html", nargs="?", help="Input bundle HTML file")
+parser.add_argument("-f", "--filetype", nargs="?", default="pdf", help="Filetype of file to extract from bundle.\
+                    Note files without a corresponding version in the [filetype] format will not be downloaded.")
+
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    if len(sys.argv) < 3 or len(sys.argv) > 4:
-        print("Usage: python hb-get.py <HumbleBundle.html> <output directory> [filetype]")
-        print("Filetype defaults to PDF and is optional.")
-    else:
-        OUTPUT_DIR = path.abspath(sys.argv[2])
-        with open(sys.argv[1]) as f:
-            soup = BeautifulSoup(f, "html.parser")
+    if args.html:
+        OUTPUT_DIR = os.path.abspath(args.output_dir)
+        with open(args.html) as fp:
+            soup = BeautifulSoup(fp, "html.parser")
 
         dl_links = soup.select(".download a[href^='https://dl.humble.com']")
         
         # Filter links by file type 
-        filetype = "PDF" if len(sys.argv) < 5 else sys.argv[4]
-        dl_links = list(filter(lambda x: x.get_text().strip() == filetype, dl_links))
+        dl_links = list(filter(lambda x: x.get_text().strip() == args.filetype, dl_links))
 
         failed_dl_count = 0
+        breakpoint()
         for link in dl_links:
             btn_text = link.get_text().strip()
             url = str(link['href'])
             filename = url.split("?")[0][22:]
             
-            filepath = path.join(OUTPUT_DIR, filename)
-            if path.exists(filepath):
+            filepath = os.path.join(OUTPUT_DIR, filename)
+            if os.path.exists(filepath):
                 print(f"Skipping... {filepath} already exists")
                 continue
             else:
@@ -34,7 +45,7 @@ if __name__ == "__main__":
 
             # TODO: add progress bar per file
             # Fetch file from web
-            file = get(url)
+            file = requests.get(url)
             if file.status_code < 200 or file.status_code > 299:
                 print(f"Failed to download {filename}. Status code: {file.status_code}", end="")
                 failed_dl_count += 1
@@ -42,14 +53,14 @@ if __name__ == "__main__":
 
             # Create output directory
             try:
-                makedirs(OUTPUT_DIR)
+                os.makedirs(OUTPUT_DIR)
             except OSError as e:
                 pass # ignore error if OUTPUT_DIR already exists
                 
             # Write to file
             try:
-                with open(filepath, "w+b") as f:
-                    f.write(file.content)
+                with open(filepath, "w+b") as fp:
+                    fp.write(file.content)
 
                 print("Done")
             except (OSError, IOError) as e:
@@ -59,3 +70,6 @@ if __name__ == "__main__":
 
         print("\nDownloads complete:")
         print(f"\t{failed_dl_count} failed downloads")
+
+    else:
+        print("hi")

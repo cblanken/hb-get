17807849bb14a4d9de2f5ea77ec8c24f5684e6fe
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-05-16 15:13:22 -0400

    Cleanup pylint warnings and add docstrings

--- hb-get/main.py
+++ hb-get/main.py
@@ -2,10 +2,9 @@
 import argparse
 import os
 import pathlib
-import sys
+from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
 import selenium_driver as sd
 
 
@@ -14,10 +13,13 @@ parser = argparse.ArgumentParser(
         description="Humble Book Bundle download program",
         epilog="")
 
-parser.add_argument("-o", "--output_dir", nargs="?", help="Output directory")
-parser.add_argument("--html", action="store_true", help="Read from HTML input file instead of scraping humblebundle.com")
-parser.add_argument("-f", "--filetype", nargs="?", default="pdf", help="Filetype of file to extract from bundle.\
-                    Note files without a corresponding version in the [filetype] format will not be downloaded.")
+parser.add_argument("-o", "--output_dir", nargs="?",
+                    help="Output directory")
+parser.add_argument("--html", action="store_true",
+                    help="Read from HTML input file instead of scraping humblebundle.com")
+parser.add_argument("-f", "--filetype", nargs="?", default="pdf",
+                    help="Filetype of file to extract from bundle. Note files without a \
+                        corresponding version in the [filetype] format will not be downloaded.")
 
 args = parser.parse_args()
 
@@ -31,7 +33,7 @@ def save_from_url(url: str, output_dir: str, filename: str):
 
     # TODO: add progress bar per file
     # Fetch file from web
-    file = requests.get(url)
+    file = requests.get(url, timeout=5)
     if file.status_code != requests.status_codes.codes.ok:
         print(f"Failed to download {filename}. Status code: {file.status_code}", end="", flush=True)
         return
@@ -39,7 +41,7 @@ def save_from_url(url: str, output_dir: str, filename: str):
     # Create output directory
     try:
         os.makedirs(output_dir)
-    except OSError as e:
+    except OSError:
         pass # ignore error if output_dir already exists
         
     # Write to file
@@ -48,28 +50,28 @@ def save_from_url(url: str, output_dir: str, filename: str):
             fp.write(file.content)
 
         print("Done")
-    except (OSError, IOError) as e:
+    except (OSError, IOError) as exc:
         print(f"Could not open {filepath}")
-        print(e)
+        print(exc)
 
 if __name__ == "__main__":
     if args.html:
-        with open(args.html) as fp:
+        with open(args.html, encoding="utf-8") as fp:
             soup = BeautifulSoup(fp, "html.parser")
 
         dl_hrefs = soup.select(".download a[href^='https://dl.humble.com']")
-        
-        # Filter links by file type 
-        filtered_hrefs = list(filter(lambda x: x.get_text().strip().lower() == args.filetype, dl_hrefs))
+
+        # Filter links by file type
+        filtered_hrefs = list(
+            filter(lambda x: x.get_text().strip().lower() == args.filetype, dl_hrefs))
 
         for i, link in enumerate(filtered_hrefs):
             pprint(link)
-            breakpoint()
             btn_text = link.get_text().strip()
-            url = str(link['href'])
-            filename = url.split("?")[0][22:]
+            URL = str(link['href'])
+            filename = URL.split("?", maxsplit=1)[0][22:]
             if args.output_dir:
-                save_from_url(url, args.output_dir, filename)
+                save_from_url(URL, args.output_dir, filename)
 
     else:
         hb = sd.HumbleDriver("https://www.humblebundle.com")
@@ -77,9 +79,8 @@ if __name__ == "__main__":
         purchases = hb.get_purchases()
         hb.select_purchase()
         download_links_by_title = hb.get_download_links_by_title(args.filetype)
-        for title, url in download_links_by_title.items():
+        for title, URL in download_links_by_title.items():
             if args.output_dir:
-                save_from_url(url, args.output_dir, f"{title}.{args.filetype}")
+                save_from_url(URL, args.output_dir, f"{title}.{args.filetype}")
             else:
-                print(f"{title}: {url})")
-
+                print(f"{title}: {URL})")
--- hb-get/selenium_driver.py
+++ hb-get/selenium_driver.py
@@ -1,15 +1,27 @@
+"""Selenium driver and utility functions for interfacing with the HumbleBundle website.
+
+Typical usage example:
+
+    hb = sd.HumbleDriver("https://www.humblebundle.com")
+    hb.login()
+    purchases = hb.get_purchases()
+    hb.select_purchase()
+    download_links_by_title = hb.get_download_links_by_title(args.filetype)
+    for title,link in download_links_by_title.items():
+        print(title, link)
+"""
 from getpass import getpass
 import os
 import sys
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 class HumbleDriver:
+    """Selenium driver class"""
     def __init__(self, url: str, max_auth_time: int = 30):
         self.base_url = url
         self.max_auth_time = max_auth_time
@@ -33,42 +45,64 @@ class HumbleDriver:
         # TODO: implement context manager __exit__
         pass
 
-    def login(self, user: str = os.environ.get("HBGET_USER"),
-              pw: str = os.environ.get("HBGET_PASS")):
+    def login(self,
+              user: str = os.environ.get("HBGET_USER"),
+              password: str = os.environ.get("HBGET_PASS")):
+        """Authenticate user on the HumbleBundle login page.
 
-        self.driver.get(self.base_url)
+        Note: It is recommended to provide these values via the default
+            environment variables HBGET_USER and HBGET_PASS.
 
-        # Click login button
-        login_btn = self.driver.find_element(By.LINK_TEXT, "Log In")
-        login_btn.click()
-
-        # Submit login form
-        form_user = self.driver.find_element(By.NAME, "username")
-        if user is None:
-            user = input("Please provide your HumbleBundle username: ")
-        form_user.send_keys(user)
-
-        form_pass = self.driver.find_element(By.NAME, "password")
-        if pw is None:
-            pw = getpass("Please provide your HumbleBundle password: ")
-        form_pass.send_keys(pw)
-
-        form_submit_btn = self.driver.find_element(By.CSS_SELECTOR, "button[type=submit]")
-        form_submit_btn.click()
-
-        # Wait for authentication to complete (including MFA prompt)
-        print(f"If MFA is enabled you'll need to provide the code...you have {self.max_auth_time} seconds.")
+        Args:
+            user: A string of the user's HumbleBundle username.
+            password: A string of the user's HumbleBundle password.
+        """
         try:
-            self.auth_wait.until(EC.none_of(EC.title_contains("Log In")))
-        except TimeoutException:
-            print("The authentication request timed out.")
-            again = input("Try again? (y/n): ")
-            if again.lower() == 'y':
-                self.login(user)
-            else:
-                sys.exit()
+            self.driver.get(self.base_url)
+
+            # Click login button
+            login_btn = self.driver.find_element(By.LINK_TEXT, "Log In")
+            login_btn.click()
+
+            # Submit login form
+            form_user = self.driver.find_element(By.NAME, "username")
+            if user is None:
+                user = input("Please provide your HumbleBundle username: ")
+            form_user.send_keys(user)
+
+            form_pass = self.driver.find_element(By.NAME, "password")
+            if password is None:
+                password = getpass("Please provide your HumbleBundle password: ")
+            form_pass.send_keys(password)
+
+            form_submit_btn = self.driver.find_element(By.CSS_SELECTOR, "button[type=submit]")
+            form_submit_btn.click()
+
+            # TODO: identify 2FA screen and reply with conditional warning/log message
+            # TODO: identify failed login screen and query user for another login attempt
+
+            # Wait for authentication to complete (including MFA prompt)
+            print("If MFA is enabled, you must provide the code.")
+            print(f"You have {self.max_auth_time} seconds.")
+            try:
+                self.auth_wait.until(EC.none_of(EC.title_contains("Log In")))
+            except TimeoutException:
+                print("The authentication request timed out.")
+                again = input("Try again? (y/n): ")
+                if again.lower() == 'y':
+                    self.login(user)
+                else:
+                    sys.exit()
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt. Exiting...")
 
     def get_purchases(self) -> list:
+        """Retrives a list of elements corresponding to the purchases of user.
+
+        Returns:
+            A list of type List[WebElement] of each purchase on a user's purchases page
+        """
+
         self.driver.get(f"{self.base_url}/home/purchases")
 
         # Wait for purchase rows to load
@@ -77,16 +111,41 @@ class HumbleDriver:
         return purchases
 
     def get_download_links(self, filetype: str):
+        """Retreive a list of download links for all items in a bundle
+
+        Args:
+            filetype: A string specifying the filetype by its extension.
+                For example: "pdf" or "epub". The links are filtered by this
+                extension and only links with the specified file extension are returned.
+
+        Returns:
+            A list of strings containing the download links 
+        """
         # Wait for rows to load with download links
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".row")))
 
         # Extract download links from row elements
-        download_elements = self.driver.find_elements(By.CSS_SELECTOR, ".download a[href^='https://dl.humble.com']")
-        download_elements = list(filter(lambda x: x.text.strip().lower() == filetype, download_elements))
+        download_elements = self.driver \
+            .find_elements(By.CSS_SELECTOR, ".download a[href^='https://dl.humble.com']")
+        download_elements = list(
+            filter(lambda x: x.text.strip().lower() == filetype, download_elements)
+        )
         download_links = [x.get_attribute("href") for x in download_elements]
         return download_links
-    
+
     def get_download_links_by_title(self, filetype: str):
+        """Retrieve links to Humble Bundle item by their titles
+
+        Args:
+            filetype: a string specifying the filetype by its extension.
+                For example: "pdf" or "epub". The links are filtered by this
+                extension and only links with the specified file extension are returned
+
+        Returns:
+            A dict mapping titles of items in a purchased bundle to their download links
+
+            # TODO: add example
+        """
         # Wait for rows to load with download links
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".row")))
 
@@ -95,7 +154,8 @@ class HumbleDriver:
         download_links_by_title = {}
         for row in row_elements:
             title = row.find_element(By.CSS_SELECTOR, ".title").text.strip()
-            href_elements = row.find_elements(By.CSS_SELECTOR, ".download a[href^='https://dl.humble.com']")
+            href_elements = row.find_elements(
+                By.CSS_SELECTOR, ".download a[href^='https://dl.humble.com']")
 
             # Check each download option for matching `filetype`
             for ele in href_elements:
@@ -104,14 +164,16 @@ class HumbleDriver:
         return download_links_by_title
 
     def select_purchase(self):
+        """Query user for selection of HumbleBundle purchases
+        """
         purchases = self.get_purchases()
         if len(purchases) == 0:
             return
 
-        for i, p in enumerate(purchases):
-            print(f"{i:>3}: {p.text}")
+        for i, purchase in enumerate(purchases):
+            print(f"{i:>3}: {purchase.text}")
 
-        while(True):
+        while True:
             sel = input(f"Select one above purchases (0-{len(purchases)}): ")
             try:
                 sel = int(sel)
@@ -122,8 +184,3 @@ class HumbleDriver:
                 break
             except ValueError:
                 continue
-
-
-
-
-
